fa96a74a49a3c4c6bf93ccfa1eb907e16c351e97
vantuan.phan <phanvantuan99hth@gmail.com>
2021-03-17 15:10:10 +0700

    excel

--- src/server.js
+++ src/server.js
@@ -6,58 +6,70 @@ const cron = require("node-cron");
 const TelegramBot = require('node-telegram-bot-api');
 const { PORT, TELEGRAM_ID, BOT_TOKEN, NODE_ENV, HEROKU_URL } = config;
 const timeTables = require('./timetables.json');
-// const ggSheet = require('./ggSheet');
-
+const ggSheet = require('./ggSheet');
+const ngrok = require('ngrok');
 
 const app = express();
 app.use(morgan(":method :url :status - :response-time ms"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) =>
+{
      return res.send("Hello world!");
 });
 
-app.get('/photo/logo', async (req, res) =>{
+app.get('/photo/logo', async (req, res) =>
+{
      return res.sendFile(`${__dirname}/public/images/botlogo.jpg`)
 })
 
 //* =============================================
 //* Error handling
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) =>
+{
      return res.status(500).json({
           status: 500,
           message: err.message,
      });
 });
 
-timeTables.forEach(time => {
-     cron.schedule(time.time, () => {
-          bot.sendMessage(TELEGRAM_ID, time.message);
-     }, {timezone: 'Asia/Bangkok'});
-})
-
 let bot;
-if(NODE_ENV === 'production'){
-     bot = new TelegramBot(BOT_TOKEN);
-     bot.setWebHook(`${HEROKU_URL}/bot/${BOT_TOKEN}`);
-}else{
-     bot = new TelegramBot(BOT_TOKEN, {polling: true});
-}
+(async function ()
+{
+     if (NODE_ENV === 'production') {
+          const url = await ngrok.connect({ port: PORT });
+          bot = new TelegramBot(BOT_TOKEN);
+          bot.setWebHook(`${url}/bot/${BOT_TOKEN}`);
+     } else {
+          bot = new TelegramBot(BOT_TOKEN, { polling: true });
+     }
 
-app.post(`/bot/:token`, (req, res) => {
-     const message = `TP_BOT xin chào..`
-     bot.sendMessage(TELEGRAM_ID, message, { parse_mode: 'HTML'});
+     timeTables.forEach(time =>
+     {
+          cron.schedule(time.time, () =>
+          {
+               bot.sendMessage(TELEGRAM_ID, time.message);
+          }, { timezone: 'Asia/Bangkok' });
+     })
+     // send message welcome
+     bot.on('message', function (msg)
+     {
+          const chatId = msg.chat.id;
+          console.log(`msg`, msg)
+          bot.sendMessage(chatId, 'TP_BOT xin chào.');
+     });
+})();
+
+app.post(`/bot/:token`, (req, res) =>
+{
+     const message = `TP_BOT xin chào..👋`;
+     bot.sendMessage(TELEGRAM_ID, message, { parse_mode: 'HTML' });
      return res.sendStatus(200);
 })
 
-// send message welcome
-bot.on('message', function (msg) {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'TP_BOT xin chào.');
-});
-
-app.all("*", (req, res) => {
+app.all("*", (req, res) =>
+{
      return res.json({
           status: 404,
           message: `Can't find ${req.originalUrl}`,
